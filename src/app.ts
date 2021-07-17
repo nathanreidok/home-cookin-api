d19d354a1e5edd56c88330db431d0fab6600b760
Nathan Reid <nathanreid@180medical.com>
2021-07-16 22:37:38 -0500

    Recipe CRUD

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 
 import express from 'express'
 import recipesApi from './routes/recipesApi'
+import cors from 'cors'
 
 const port = process.env.HOME_COOKIN_API_PORT || 3001
 const app = express()
 
+const corsOptions = {
+    origin: 'http://localhost:3000'
+  }
+
 app.use(express.json())
+app.use(cors(corsOptions))
 
 app.use('/recipes', recipesApi)
 
@@ -13,6 +19,4 @@ app.get('/', (req, res) => {
     res.send('Hello')
 })
 
-
-
-app.listen(port, () => console.log('running...'))
+app.listen(port, () => console.log(`Running on port ${port}`))
--- src/routes/recipesApi.ts
+++ src/routes/recipesApi.ts
@@ -4,15 +4,17 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send([1,2,3])
+    var recipes = recipeData
+    res.send(recipes || [])
 })
 
 router.get('/:id', (req, res) => {
     var id = req.params.id
-    // req.query
-    res.send(id)
 
-    if (false) res.status(404).send('Could not find recipe')
+    var recipe = recipeData.find(r => r.id == id)
+
+    if (!recipe) return res.status(404).send('Could not find recipe')
+    res.send(recipe)
 })
 
 router.post('/', (req, res) => {
@@ -28,38 +30,82 @@ router.post('/', (req, res) => {
         return
     }
 
-    Joi.valid()
-    if (!req.body.name) {
-        res.status(400).send('Name is required')
-        return
-    }
     const recipe = {
-        id: 1,
+        id: Math.max.apply(Math, recipeData.map(r => parseInt(r.id))).toString(),
         name: req.body.name,
-        directions: req.body.directions
+        directions: req.body.directions,
+        ingredients: req.body.ingredients,
     }
-    //add recipe here
+    recipeData.push(recipe)
+
     res.send(recipe)
 })
 
 router.put('/api/recipes/:id', (req, res) => {
-    //Look up the recipe
-    // it not existing, return 404
+    var id = req.params.id
 
-    // Validate
-    // If invalid, return 400 - Bad request
+    var recipe = recipeData.find(r => r.id == id)
 
-    // Update course
-    // Return the updated course
+    if (!recipe) return res.status(404).send('Could not find recipe')
+    
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        directions: Joi.string().required(),
+    })
+
+    const result = schema.validate(req.body)
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message)
+        return
+    }
+
+    recipe!.name = req.body.name
+    recipe!.directions = req.body.directions
+    recipe!.ingredients = req.body.ingredients
+
+    res.send(recipe)
 })
 
 router.delete('/api/recipes/:id', (req, res) => {
     var id = req.params.id
-    //if not existing, return 404
 
-    // delete
+    var index = recipeData.findIndex(r => r.id == id)
 
-    //return the recipe
+    if (!(index >= 0)) return res.status(404).send('Could not find recipe')
+
+    recipeData = recipeData.splice(index, 1)
+
+    res.send()
 })
 
 export default router
+
+let recipeData = ['1','2','3','4','5','6','7','8','9','10'].map(value => ({
+    id: value,
+    name: 'Cake',
+    directions: 'Mix it and bake it.',
+    ingredients: [{
+        recipeId: value,
+        ingredientId: 1,
+        ingredient: 'Eggs',
+        qtyNumerator: 1,
+        qtyDenominator: 1,
+    },{
+        recipeId: value,
+        ingredientId: 2,
+        ingredient: 'Milk',
+        qtyNumerator: 1,
+        qtyDenominator: 2,
+        measurementId: 3,
+        measurement: 'Tablespoon',
+    },{
+        recipeId: value,
+        ingredientId: 3,
+        ingredient: 'Sugar',
+        qtyNumerator: 5,
+        qtyDenominator: 2,
+        measurementId: 2,
+        measurement: 'Cup'
+    }],
+}))
